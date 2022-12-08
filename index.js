b155b578b7ea429aaa36a1a9f1b0cb21408b0875
tabond <30688315+tabond@users.noreply.github.com>
2022-12-07 19:37:45 -0800

    adding returns

--- index.js
+++ index.js
@@ -3,45 +3,67 @@ const express = require("express"),
 const app = express();
 app.use(morgan("common"));
 
-let topTenMovies = [
+let users = [
   {
-    title: "Pulp Fiction",
+    name: "",
+    age: "",
+    id: "",
+    userName: "",
+  },
+];
+
+let movies = [
+  {
+    Title: "Pulp Fiction",
     Year: "1994",
-    rating: "1",
+    Rating: "1",
+    Genre: {
+      Name: "Thriller",
+      blurb: "blobblobblob",
+    },
+    Director: {
+      Name: "",
+      Bio: "",
+      Birth_year: "",
+      Death_year: "",
+    },
   },
   {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    Year: "2001",
-    rating: "2",
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    year: "2001",
+    Rating: "2",
+    Genre: {
+      Name: "Fantacy",
+    },
   },
   {
-    title: "The Lord of the Rings: The Return of the King",
+    Title: "The Lord of the Rings: The Return of the King",
     Year: "2003",
-    rating: "3",
+    Rating: "3",
   },
   {
     title: "Forrest Gump",
-    Year: "1994",
+    year: "1994",
     rating: "4",
   },
   {
     title: "Fight Club",
-    Year: "1999",
+    year: "1999",
     rating: "5",
   },
   {
     title: "Inception",
-    Year: "2010",
+    year: "2010",
     rating: "6",
   },
   {
     title: "Star Wars: Episode V - The Empire Strikes Back",
-    Year: "1980",
+    year: "1980",
     rating: "7",
   },
   {
     title: "The Matrix",
-    Year: "1999",
+    year: "1999",
     rating: "8",
   },
   {
@@ -51,18 +73,66 @@ let topTenMovies = [
   },
   {
     title: "The lion King",
-    Year: "1994",
+    year: "1994",
     rating: "10",
   },
 ];
 
 app.get("/", (req, res) => {
-  res.send("welcome to myFlix");
+  res.send("welcome to myFlix!!");
 });
 
+//Returns all movies to user
 app.get("/movies", (req, res) => {
-  res.json(topTenMovies);
+  res.status(200).json(movies);
 });
+
+//return all movie data on single movie by title
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params,
+    movie = movies.find((movie) => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie");
+  }
+});
+//Return data about a genre (description) by name/title (e.g., “Thriller”);
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params,
+    genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre");
+  }
+});
+
+//Return data about a director (bio, birth year, death year) by name;
+
+//Allow new users to register;
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = "Missing name in request body";
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//Allow users to update their user info (username);
+
+//Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later);
+
+//Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later);
+
+//Allow existing users to deregister (showing only a text that a user email has been removed—more on this later).
 
 app.use(express.static("public"));
 

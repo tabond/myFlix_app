c7f02f1ae08935649be1ce17180aad6940dedb17
tabond <30688315+tabond@users.noreply.github.com>
2022-12-09 11:44:25 -0800

    api calls

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express"),
-  morgan = require("morgan");
-const app = express();
-app.use(morgan("common"));
+  app = express(),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
+
+app.use(bodyParser.json());
 
 let users = [
   {
-    name: "",
+    name: "Tom Bond",
     age: "",
-    id: "",
-    userName: "",
+    favouriteMovie: [],
+    id: 1,
+  },
+  {
+    name: "Neil Smith",
+    age: "",
+    favouriteMovies: ["Pulp Fiction"],
+    id: 2,
   },
 ];
 
@@ -22,7 +30,7 @@ let movies = [
       blurb: "blobblobblob",
     },
     Director: {
-      Name: "",
+      Name: "Quentin Tarantino",
       Bio: "",
       Birth_year: "",
       Death_year: "",
@@ -111,28 +119,92 @@ app.get("/movies/genre/:genreName", (req, res) => {
 });
 
 //Return data about a director (bio, birth year, death year) by name;
+app.get("/movies/director/:directorName", (req, res) => {
+  const { directorName } = req.params,
+    director = movies.find(
+      (movie) => movie.Director.Name === directorName
+    ).Director;
 
-//Allow new users to register;
-app.post("/users", (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.name) {
-    const message = "Missing name in request body";
-    res.status(400).send(message);
+  if (director) {
+    res.status(200).json(director);
   } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
+    res.status(400).send("no such Director");
   }
 });
 
-//Allow users to update their user info (username);
+//Allow new users to register create
+app.post("/users", (req, res) => {
+  const newUser = req.body;
 
-//Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later);
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("Missing name in request body");
+  }
+});
 
-//Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later);
+//Allow users to update their user info (username) Update
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params,
+    updatedUser = req.body;
 
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("No such user");
+  }
+});
+
+// create Allow users to add a movie to their list of favorites (showing only a text that a movie has been /added—more on this later); not working!!
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favouriteMovies.push(movieTitle);
+    res
+      .status(200)
+      .send(movieTitle + " has been added to user " + id + "s array");
+  } else {
+    res.status(400).send("No such user");
+  }
+});
+//Delete (variables not pulling into text) Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later);
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favouriteMovies = user.favouriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(movieTitle + " has been removed from user " + id + "s array");
+  } else {
+    res.status(400).send("No such user");
+  }
+});
 //Allow existing users to deregister (showing only a text that a user email has been removed—more on this later).
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id == id);
+    res.status(200).send("user " + id + " has been deleted");
+  } else {
+    res.status(400).send("No such user");
+  }
+});
 
 app.use(express.static("public"));
 

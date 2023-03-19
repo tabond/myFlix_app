d41f8b1e449b9585493b6b829fcca13eb2571507
tabond <30688315+tabond@users.noreply.github.com>
2023-03-18 17:47:19 -0700

    mistaken duplication

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken"),
 require("./passport"); //your local passport file
 
 let generateJWTToken = (user) => {
-  return jwt.sign(use, jwtSecret, {
+  return jwt.sign(user, jwtSecret, {
     subject: user.Username, //This is the username you're encoding in the JWT
     expiresIn: "7d", // This specifies that the token will expire in 7 days
     algorithm: "HS256", // This is the algorithm used to "sign" or  encodethe values of the JWT
